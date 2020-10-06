d226d68e255171b81cd2b48e71cd432829a08d2f
v8196566 <v8196566@live.tees.ac.uk>
2020-10-06 10:43:46 +0100

    Part 2

--- EmployeeManager/EmployeeManager/Program.cs
+++ EmployeeManager/EmployeeManager/Program.cs
@@ -4,13 +4,14 @@ namespace EmployeeManager
 {
     class Program
     {
-        public const double hourlyRate = 9.50;
+        public const double rate = 9.50;
 
         static void Main(string[] args)
         {
             Console.Write("Hours Worked: ");
             int hours = int.Parse(Console.ReadLine());
-            Console.WriteLine("The weekly wage is {0:C2}", (hours * hourlyRate));
+            double price = (hours > 40) ? (rate * 40 + 14.25 * (hours - 40)) : (rate * hours);
+            Console.WriteLine("The weekly wage is {0:C2}.", price);
         }
     }
 }
