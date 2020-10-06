4a2dc57c388212b0098acc67afd41414e4efc607
Jason196 <v8196566@live.tees.ac.uk>
2020-10-06 12:41:54 +0100

    completed?

---
+++ EmployeeManager/EmployeeManager/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmployeeManager
+{
+    public class Employee
+    {
+        private string name;
+        private string id;
+        private int hours;
+        private double rate;
+
+        public Employee(string name, string id, int hours, double rate = 9.50)
+        {
+            this.name = name;
+            this.id = id;
+            this.hours = hours;
+            this.rate = rate;
+        }
+
+        public static bool isValidName(string input)
+        {
+            return input.Length > 0 && input.Length <= 50;
+        }
+
+        public static bool isValidID(string input)
+        {
+            string regex = @"^[A-Z]\d{2}$";
+            return Regex.IsMatch(input, regex) ? true : false;
+        }
+
+        public static bool isValidHours(int input)
+        {
+            return input > 0 && input <= 100;
+        }
+
+        public double calcWage()
+        {
+            return (hours > 40) ? (rate * 40 + 14.25 * (hours - 40)) : (rate * hours);
+        }
+
+        public string ToString()
+        {
+            return name + " (" + id + "). Wage: " + calcWage();
+        }
+    }
+}
--- EmployeeManager/EmployeeManager/Program.cs
+++ EmployeeManager/EmployeeManager/Program.cs
@@ -1,17 +1,109 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EmployeeManager
 {
     class Program
     {
-        public const double rate = 9.50;
+        public static Dictionary<int,Employee> employees = new Dictionary<int, Employee>();
 
         static void Main(string[] args)
         {
-            Console.Write("Hours Worked: ");
+            bool run = true;
+            while (run)
+            {
+                Console.Clear();
+                Console.WriteLine("1.Add Employee\n2.List Employees (with the positon number inthe list)\n3.Remove Employee (based on a positon number provided by the user)");
+                int opt = int.Parse(Console.ReadLine());
+
+                switch (opt)
+                {
+                    case 1:
+                        Console.Clear();
+                        addEmployee();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        listEmployee();
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        removeEmployee();
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        run = false;
+                        break;
+                }
+            }
+
+            //double price = (hours > 40) ? (rate * 40 + 14.25 * (hours - 40)) : (rate * hours);
+            //Console.WriteLine("The weekly wage is {0:C2}.", price);
+        }
+
+        public static void addEmployee()
+        {
+            Console.Write("Input Employee name: ");
+            string name = Console.ReadLine();
+            while (!Employee.isValidName(name))
+            {
+                Console.Write("Invalid name, try again: ");
+                name = Console.ReadLine();
+            }
+            Console.Write("Input Employee id: ");
+            string id = Console.ReadLine();
+            while (!Employee.isValidID(id))
+            {
+                Console.Write("Invalid id, try again: ");
+                id = Console.ReadLine();
+            }
+            Console.Write("Input Hours worked: ");
             int hours = int.Parse(Console.ReadLine());
-            double price = (hours > 40) ? (rate * 40 + 14.25 * (hours - 40)) : (rate * hours);
-            Console.WriteLine("The weekly wage is {0:C2}.", price);
+            while (!Employee.isValidHours(hours))
+            {
+                Console.Write("Invalid name, try again: ");
+                hours = int.Parse(Console.ReadLine());
+            }
+            Employee emp = new Employee(name, id, hours);
+            employees.Add(employees.Count + 1, emp);
+        }
+
+        public static void listEmployee()
+        {
+            if (employees.Count == 0)
+            {
+                Console.WriteLine("No employees.");
+                return;
+            }
+            for (int i = 1; i <= employees.Count; i++)
+            {
+                Employee test = null;
+                employees.TryGetValue(i, out test);
+                Console.WriteLine(i + ". " + test.ToString());
+            }
+        }
+
+        public static void removeEmployee()
+        {
+            listEmployee();
+            if (employees.Count == 0)
+            {
+                return;
+            }
+            Console.Write("Number of employee to remove: ");
+            int id = int.Parse(Console.ReadLine());
+            if (employees.ContainsKey(id))
+            {
+                employees.Remove(id);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Employee.");
+                return;
+            }
         }
     }
 }
