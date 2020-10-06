a391ab1318d2ec5eb254e615ae06e038225ccb53
v8196566 <v8196566@live.tees.ac.uk>
2020-10-06 10:18:38 +0100

    Added EmployeeManager project

---
+++ EmployeeManager/EmployeeManager/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeManager
+{
+    class Program
+    {
+        public const double hourlyRate = 9.50;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Hours Worked: ");
+            int hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("The weekly wage is {0:C2}", (hours * hourlyRate));
+        }
+    }
+}
